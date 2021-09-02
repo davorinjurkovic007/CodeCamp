e2bf371e1dea219327141bc5b1fa7fced8bc20fe
Davorin <davorin.jurkovic@gmail.com>
2021-09-02 23:44:28 +0200

    Creating Association APIs

--- Data/CampProfile.cs
+++ Data/CampProfile.cs
@@ -15,9 +15,13 @@ namespace CoreCodeCamp.Data
                 .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
                 .ReverseMap();
 
-            CreateMap<Talk, TalkModel>();
+            CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Camp, opt => opt.Ignore())
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
 
-            CreateMap<Speaker, SpeakerModel>();
+            CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
         }
     }
 }
