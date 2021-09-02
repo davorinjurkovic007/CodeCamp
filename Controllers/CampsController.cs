be42e53aa906a7e17a08edad7375546db44fe931
Davorin <davorin.jurkovic@gmail.com>
2021-09-02 22:00:42 +0200

    Modifying Data

--- Controllers/CampsController.cs
+++ Controllers/CampsController.cs
@@ -3,6 +3,7 @@ using CoreCodeCamp.Data;
 using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,18 @@ using System.Threading.Tasks;
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository repository;
         private readonly IMapper mapper;
+        private readonly LinkGenerator linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper mapper)
+        public CampsController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
         {
             this.repository = repository;
             this.mapper = mapper;
+            this.linkGenerator = linkGenerator;
         }
 
         [HttpGet]
@@ -74,6 +78,89 @@ namespace CoreCodeCamp.Controllers
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure:" + ex.Message);
             }
+        }
+
+        public async Task<ActionResult<CampModel>> Post(CampModel model)
+        {
+            try
+            {
+                var existing = await repository.GetCampAsync(model.Moniker);
+                if(existing != null)
+                {
+                    return BadRequest("Moniker in Use");
+                }
+
+                var location = linkGenerator.GetPathByAction("Get", "Camps", new { moniker = model.Moniker });
+
+                if(string.IsNullOrEmpty(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+
+                // Create a new camp
+                var camp = mapper.Map<Camp>(model);
+                repository.Add(camp);
+                if (await repository.SaveChangesAsync())
+                {
+                    return Created($"/api/camps/{camp.Moniker}", mapper.Map<CampModel>(camp));
+                }
+            }            
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure:" + ex.Message);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{moniker}")]
+        public async Task<ActionResult<CampModel>> Put(string moniker, CampModel model)
+        {
+            try
+            {
+                var oldCamp = await repository.GetCampAsync(moniker);
+                if(oldCamp == null)
+                {
+                    return NotFound($"Could not find camp with monike of {moniker}");
+                }
+
+                mapper.Map(model, oldCamp);
+
+                if(await repository.SaveChangesAsync())
+                {
+                    return mapper.Map<CampModel>(oldCamp);
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure:" + ex.Message);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{moniker}")]
+        public async Task<IActionResult> Delete(string moniker)
+        {
+            try
+            {
+                var oldCamp = await repository.GetCampAsync(moniker);
+                if (oldCamp == null) return NotFound();
+
+                repository.Delete(oldCamp);
+
+                if(await repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure:" + ex.Message);
+            }
+
+            return BadRequest("Failed to delete the camp");
         }
     }
 }
